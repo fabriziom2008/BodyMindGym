01f9c76cbbfe75069fabbc41be3a34dca3cfb401
Fabrízio <100778214+fabriziom2008@users.noreply.github.com>
2023-03-12 16:36:50 -0300

    Create projetogym.py

---
+++ projetogym.py
@@ -0,0 +1,17 @@
+print('Bem vind@ a Body Mind!')
+horario = int(input('Quantas horas semanais você quer fazer?'))
+plano1 = 2
+plano2 = 3
+plano3 = 5
+plano4 = 999999
+
+if horario <=2:
+    print('Te recomendamos o Plano 1, com 2 horas semanais!')
+if horario > 5:
+    print('Te recomendamos o Plano 4, com quantas horas você quiser!')
+if horario == 3:
+    print('Te sugerimos o Plano 2, com 3 horas semanais!')
+if horario == 5:
+    print('Te sugerimos o Plano 3, com 5 horas semanais!')
+if horario == 4:
+    print('Te sugerimos o Plano 3, com 5 horas semanais!')
